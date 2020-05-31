76309eed647b33f3448ca4a48bb1516b620bab33
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-31 12:47:41 +0300

    successfully test all methods

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -7,50 +7,75 @@ import static org.junit.Assert.*;
 public class HeroTest {
 
     @Test
-    public void getName() {
+    public void getName_returnsCorrectName_true() {
+        Hero hero = hero();
+        assertEquals("Ali", hero.getName());
     }
 
     @Test
-    public void setName() {
+    public void setName_setsCorrectName_true() {
+        Hero hero = hero();
+        assertEquals(1, hero.getAge());
     }
 
     @Test
-    public void getAge() {
+    public void getAge_collectsCorrectAge_true() {
+        Hero hero = hero();
+        assertEquals("fire", hero.getSpecialPowers());
     }
 
     @Test
-    public void setAge() {
+    public void setAge_setsCorrectAge_true() {
+        Hero hero = new Hero("Ali", 1, "fire", "women", "hey");
+        assertEquals("women", hero.getWeakness());
     }
 
     @Test
-    public void getSpecialPowers() {
+    public void getSpecialPowers_returnsCorrectly_true() {
+        Hero hero = hero();
+        assertEquals("hey", hero.getSquad());
     }
 
     @Test
-    public void setSpecialPowers() {
+    public void setSpecialPowers_setsAppropriateName_true() {
+        Hero hero = hero();
+        hero.setSpecialPowers("brick");
+        assertEquals("brick", hero.getSpecialPowers());
     }
 
     @Test
-    public void getWeakness() {
+    public void getWeakness_collectsCorrectWeakness_true() {
+        Hero hero = new Hero("Ali", 1, "fire", "women", "hey");
+        assertEquals("women", hero.getWeakness());
     }
 
     @Test
-    public void setWeakness() {
+    public void setWeakness_setsCorrectWeakness_true() {
+        Hero hero = new Hero("Ali", 1, "fire", "women", "hey");
+        hero.setWeakness("madame");
+        assertEquals("madame", hero.getWeakness());
     }
 
     @Test
-    public void getHeroes() {
+    public void Hero_instantiateCorrectly_true() {
+        Hero hero = hero();
+        assertNotNull(hero);
     }
 
     @Test
     public void findById() {
+        Hero hero = hero();
+        assertEquals(1, Hero.findById(hero.getId()).getId());
     }
 
     @Test
     public void getSquad() {
+        Hero hero = hero();
+        assertEquals("hey", hero.getSquad());
     }
 
-    @Test
-    public void getId() {
+    //helper method
+    public Hero hero(){
+        return new Hero("Ali", 1, "fire", "women", "hey");
     }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -12,25 +12,45 @@ public class SquadTest {
 
     @Test
     public void setName() {
+        Squad squad = squad();
+        squad.setName("Abdi");
+        assertEquals("Abdi", squad.getName());
     }
 
     @Test
     public void getDedicatedToFight() {
+        Squad squad = squad();
+        assertEquals("Racism", squad.getDedicatedToFight());
     }
 
     @Test
     public void setDedicatedToFight() {
+        Squad squad = squad();
+        squad.setDedicatedToFight("tribalism");
+        assertEquals("tribalism", squad.getDedicatedToFight());
     }
 
     @Test
     public void getMaxSize() {
+        Squad squad = squad();
+        assertEquals(5, squad.getMaxSize());
     }
 
     @Test
     public void setMaxSize() {
+        Squad squad = squad();
+        squad.setMaxSize(6);
+        assertEquals(6, squad.getMaxSize());
     }
 
     @Test
     public void getSquads() {
+        Squad squad = squad();
+        assertTrue(squad instanceof Squad);
+    }
+
+    //helper method
+    Squad squad(){
+        return new Squad("Achievers","Racism", 5);
     }
 }
