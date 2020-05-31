552a7031c8c6f5b37d4489dada15f255a3633e9b
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-31 09:59:00 +0300

    add POJO to hero.java

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,71 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPowers;
+    private String weakness;
+    private static ArrayList<Hero> heroes = new ArrayList();
+    private int id;
+    private String heroSquad;
+
+    public Hero(String name, int age, String specialPowers, String weakness, String heroSquad){
+        this.name = name;
+        this.age = age;
+        this.specialPowers = specialPowers;
+        this.weakness = weakness;
+        this.heroes.add(this);
+        this.id = heroes.size();
+        this.heroSquad = heroSquad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPowers() {
+        return specialPowers;
+    }
+
+    public void setSpecialPowers(String specialPowers) {
+        this.specialPowers = specialPowers;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public static ArrayList<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public static Hero findById(int id){
+        return heroes.get(id -1);
+    }
+
+    public String getSquad(){
+        return heroSquad;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
